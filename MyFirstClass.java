e97dedc1580315ddc0c9d7916130c4236b2d00d5
ann1777 <angela.harkov@gmail.com>
2022-02-15 16:26:11 +0200

    An instance of the second class has created in the main method

--- MyFirstClass.java
+++ MyFirstClass.java
@@ -6,5 +6,10 @@ public class MyFirstClass {
     public static String returnGreeting(String courseName) {
         return String.format("~~~ %s course 2022 is started ~~~%n", courseName);
     }
-
+    //в методе main создан экземпляр второго класса
+    public static void main(String[] args) {
+        System.out.printf(MyFirstClass.returnGreeting("Java AQA"));
+        MySecondClass secondClassInstance = new MySecondClass();
+        System.out.println(secondClassInstance.returnNumberOfStudentsPresent());
+    }
 }
