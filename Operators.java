42b4dda9ad29ec83f3af14f561055290c0935b09
Angela Potapchuk <angela.harkov@gmail.com>
2022-02-16 11:48:36 +0200

    Values have benn summed in pairs

--- Operators.java
+++ Operators.java
@@ -3,12 +3,15 @@ package com.company;
 
 public class Operators {
 
-    // Assigning values to 4 fields of type int
+    // Assigning values of type int to 4 fields
     public static void main(String args[]){
         int a = 10;
         int b = 40;
         int c = 80;
         int d = 120;
+        //Summing values in pairs
+        int e = a + d;
+        int g = b + c;
     }
 
 }
