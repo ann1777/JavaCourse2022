af9383a741beb13e9b1af0750727b3aff98b0e23
Angela Potapchuk <angela.harkov@gmail.com>
2022-02-16 11:45:10 +0200

    Assigned values of type int to 4 fields

---
+++ Operators.java
@@ -0,0 +1,15 @@
+package com.company;
+
+
+public class Operators {
+
+    // Assigning values to 4 fields of type int
+    public static void main(String args[]){
+        int a = 10;
+        int b = 40;
+        int c = 80;
+        int d = 120;
+    }
+
+}
+
