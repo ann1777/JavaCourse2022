c62d93ec1c7260d0d6f749466a23bdfcaf6df243
Angela Potapchuk <angela.harkov@gmail.com>
2022-05-22 16:32:36 +0300

    Delete files

--- src/lesson9/Main.java
+++ src/lesson9/Main.java
@@ -1,11 +1,1 @@
-package lesson9;
 
-public class Main {
-    public static void main(String[] args) {
-        StringArray array1 = new StringArray();
-
-        array1.input1();
-
-        System.out.println(array1.process());
-    }
-}
