3f58de6812374a1384af1a9e3f44e4704fcff6c7
Angela Potapchuk <angela.harkov@gmail.com>
2022-05-22 20:45:51 +0300

    Initialized new void input1() in the StringArrayStorage class.

--- src/lesson9/StringArray.java
+++ src/lesson9/StringArray.java
@@ -1,5 +1,12 @@
 package lesson9;
 
+import java.util.Scanner;
+
 public class StringArray {
     public String[] StringArray1;
+
+    public void input1() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input the number of elements of StringArray: ");
+    }
 }
