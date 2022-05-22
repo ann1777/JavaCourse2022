6bb71431700f16714a31e92642ddd35a04a44508
Angela Potapchuk <angela.harkov@gmail.com>
2022-05-22 20:56:52 +0300

    New set of unique characters of the string created. Extra spaces removed from the string and all char of the string added to the new ArrayList.

--- src/lesson9/StringArray.java
+++ src/lesson9/StringArray.java
@@ -1,6 +1,9 @@
 package lesson9;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class StringArray {
     public String[] StringArray1;
@@ -19,5 +22,21 @@ public class StringArray {
             StringArray1[i] = scanner.nextLine(); // inputting n StringArrays from the keyboard
         }
     }
+
+    public Set<Character> process() {
+        Set<Character> sett = new HashSet<>(); // initializing a set of unique characters
+        int count = 0; // initializing a counter of String containing all even characters
+
+        for (String s : StringArray1) {
+            String str1 = new String(s);
+            str1 = str1.replace(" ", ""); // removed str1 extra spaces
+
+            ArrayList<Character> ch = new ArrayList<>(str1.length());
+            for (char char1 : str1.toCharArray()) ch.add(char1); // getting list of str1 Characters
+
+        }
+        return sett;
+    }
+
 }
 
