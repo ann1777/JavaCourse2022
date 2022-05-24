237adc744b0023bb5486afb9f7a66fd31df8f26b
Angela Potapchuk <angela.harkov@gmail.com>
2022-05-24 11:17:42 +0300

    A public void input1 for entering array of Strings from the keyboard was added to StringArray class and has been called from the Main class

--- src/lesson9/StringArray.java
+++ src/lesson9/StringArray.java
@@ -3,6 +3,20 @@ package lesson9;
 import java.util.*;
 
 public class StringArray {
-    public String[] StringArray1;
+    public String[] StringArray1;//Declaring a new StringArray1
 
+    public void input1() {
+        Scanner scanner = new Scanner(System.in);//Initializing new reading from display scanner
+        System.out.println("Input the number of elements of the StringArray: ");
+
+        String n1 = scanner.nextLine();// Reading the value from the scanner
+        int n = Integer.parseInt(n1);// Initializing numbers of StringArray n
+
+        StringArray1 = new String[n];// Initializing a new StringArray1
+        for (int i=0; i<n; i++) {
+            System.out.println("Input the " + (i + 1) + " element of StringArray: ");
+
+            StringArray1[i] = scanner.nextLine();//inputting n StrtingArray from the 4keyboard
+        }
+    }
 }
