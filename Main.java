eb0a6f42e659f0e8691b77b4a8c8b616fbd44e56
Angela Potapchuk <angela.harkov@gmail.com>
2022-05-24 17:38:56 +0300

    A public void input1 for entering Strings from the keyboard was added to StringArray class.

--- Main.java
+++ Main.java
@@ -3,5 +3,7 @@ import com.company.src.lesson9.StringArray;
 public class Main {
     public static void main(String[] args) {
         StringArray array1 = new StringArray();
+
+        array1.input1();
     }
 }
--- src/lesson9/StringArray.java
+++ src/lesson9/StringArray.java
@@ -4,5 +4,21 @@ import java.util.*;
 
 public class StringArray {
     public String[] StringArray1;//Declaring a new StringArray1
+
+    public void input1() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input the number of elements of StringArray: ");
+
+
+        String n1 = scanner.nextLine();
+        int n = Integer.parseInt(n1); // initializing numbers n of StringArray
+
+        StringArray1 = new String[n]; // initializing a new StringArray
+        for (int i = 0; i < n; i++) {
+            System.out.println("Input the " + (i + 1) + " element of the String Array: ");
+
+            StringArray1[i] = scanner.nextLine(); // inputting n StringArrays from the keyboard
+        }
+    }
 }
 
