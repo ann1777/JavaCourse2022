a6be0ab139dffca0ce54393735a332ab871ae48b
Angela Potapchuk <angela.harkov@gmail.com>
2022-05-24 10:41:00 +0300

    Created class Main and class StringArray in the lesson9 package. Initialized new StringArray1 in the StringArray class. New example of the StringArray created in the Main class

--- src/lesson9/Main.java
+++ src/lesson9/Main.java
@@ -3,8 +3,5 @@ package lesson9;
 public class Main {
     public static void main(String[] args) {
         StringArray array1 = new StringArray();
-
-        array1.input1();
-        System.out.println(array1.process());
     }
 }
