c1a7f489e0366215d9f57b923c9238d56efc2541
Angela Potapchuk <angela.harkov@gmail.com>
2022-05-24 14:21:57 +0300

    merge history commits

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
-    int i;
-    public Main() {
-        i = 10;
+    public static void main(String[] args) {
+
+        StringArray array1 = new StringArray();
+        array1.input1();
+
+        System.out.println(array1.process());
     }
 }
