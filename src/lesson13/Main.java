ff899e21f06c799c92023dca430a83352d890116
Angela Potapchuk <angela.harkov@gmail.com>
2022-06-01 14:50:51 +0300

    Initialized parameters for every class in Planet.java. Created println method for printing all enum values and theirs parameters to the screen.

--- src/lesson13/Main.java
+++ src/lesson13/Main.java
@@ -1,9 +1,21 @@
 package lesson13;
 
 
+import java.util.Arrays;
+
+
 public class Main {
     public static void main(String[] args) {
+        Planet[] p = new Planet[]{Planet.MERCURY, Planet.VENUS, Planet.EARTH, Planet.MARS, Planet.JUPITER, Planet.SATURN,
+                Planet.URANUS, Planet.NEPTUN, Planet.PLUTO};
 
-        System.out.println(Planet.values());
+        System.out.println(Arrays.toString(Planet.values()));
+
+        for (Planet planet : p) {
+            System.out.println(planet.name() + ": serialNumber " + planet.serialNumber() + ", pathToPrev " + planet.pathToPrev() +
+                    ", pathToTheSun " + planet.pathToTheSun() + ". radius " + planet.radius() + ", Planet prev is " + planet.prev() +
+                    ", Planet next is " + planet.next);
+        }
     }
 }
+
--- src/lesson13/Planet.java
+++ src/lesson13/Planet.java
@@ -1,44 +1,48 @@
 package lesson13;
 
-public enum Planet{
-    MERCURY(1, 0, 58, 2439, null, VENUS),
-    VENUS(2, 50, 108, 6051.8, MERCURY, EARTH),
-    EARTH(3, 42, 150, 6378.1, VENUS, MARS),
-    MARS(4, 78, 228, 3397.2, EARTH, JUPITER),
-    JUPITER(5, 550, 778, 7149.2, MARS, SATURN),
-    SATURN(6, 651, 1429, 6026.8, JUPITER, URANUS),
-    URANUS(7, 1446, 2875, 255.9, SATURN, NEPTUN),
-    NEPTUN(8, 1622, 4497, 2474.6, URANUS, PLUTO),
-    PLUTO(9, 1416, 5913, 1188.14, NEPTUN, null);
+public enum Planet {
+
+    MERCURY(1, 0, 58, 2439, null),
+    VENUS(2, 50, 108, 6051.8, MERCURY),
+    EARTH(3, 42, 150, 6378.1, VENUS),
+    MARS(4, 78, 228, 3397.2, EARTH),
+    JUPITER(5, 550, 778, 7149.2, MARS),
+    SATURN(6, 651, 1429, 6026.8, JUPITER),
+    URANUS(7, 1446, 2875, 255.9, SATURN),
+    NEPTUN(8, 1622, 4497, 2474.6, URANUS),
+    PLUTO(9, 1416, 5913, 1188.14, NEPTUN);
 
     private final int serialNumber;// номер по порядку от солнца (для спутников применяется номер планеты)
     private final double pathToPrev;// удаленность от предыдущей планеты (для меркурия 0)
     private final double pathToTheSun; // удаленность от солнца (вычисляется в конструкторе)
     private final double radius;// радиус
-    private Planet prev;// предыдущая планета
+    private final Planet prev;// предыдущая планета
     public Planet next;// следующая планета
 
-    Planet(int serialNumber, double pathToPrev, double pathToTheSun, double radius, Planet prev, Planet next) {
-        this.serialNumber = serialNumber;
-        this.pathToPrev = pathToPrev;
-        this.pathToTheSun = prev.pathToTheSun + this.pathToPrev;
-        this.radius = radius;
-        this.prev = prev;
-        this.next = SwitchNextPlanet();
-        prev.next = this;
-    }
-
-    Planet(int serialNumber, double pathToPrev, double pathToTheSun, double radius, Planet next) {
+    Planet(int serialNumber, double pathToPrev, double pathToTheSun, double radius, Planet prev) {
         this.serialNumber = serialNumber;
         this.pathToPrev = pathToPrev;
         this.pathToTheSun = pathToTheSun;
         this.radius = radius;
-        this.prev = null;
-        this.next = SwitchNextPlanet();
-        prev.next = this;
+        this.prev = prev;
+        if (prev != null) {
+            prev.next = this;
+        }
     }
 
-    protected Planet SwitchNextPlanet() {
-        return this.next;
+    public int serialNumber() {
+        return serialNumber;
+    }
+    public double pathToPrev() {
+        return pathToPrev;
+    }
+    public double pathToTheSun() {
+        return pathToTheSun;
+    }
+    public double radius() {
+        return radius;
+    }
+    public Planet prev() {
+        return prev;
     }
 }
