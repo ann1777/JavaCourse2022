3f1bda33be80fc2e86ceecc0ea57c11740422765
Angela Potapchuk <angela.harkov@gmail.com>
2022-06-05 23:47:51 +0300

    Five students created in the main class and tested class methods.

--- lesson14/src/main/java/incapsulation/Main.java
+++ lesson14/src/main/java/incapsulation/Main.java
@@ -2,6 +2,16 @@ package incapsulation;
 
 public class Main {
     public static void main(String[] args) {
-
+        Student student1 = new Student(111, "Ivan", "Potap");
+        Student student2 = new Student(234, "Kirill", "Gnatenko");
+        Student student3 = new Student(345, "Olesya", "Bondarenko");
+        Student student4 = new Student(456, "Yulia", "Zhurba");
+        Student student5 = new Student(567, "Kirill", "Nechitailo");
+        student1.setID(123);
+        student1.setName("Tanya");
+        student1.setLastname("Potapchuk");
+        System.out.println(student1.getID());
+        System.out.println(student1.getName());
+        System.out.println(student1.getLastname());
     }
 }
