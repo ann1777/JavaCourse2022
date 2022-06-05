9869c7c0e947c8771b472a6d052a3ef98bcafd13
Angela Potapchuk <angela.harkov@gmail.com>
2022-06-06 00:38:09 +0300

    Added addStudent, removeStudent, renameStudent, changeGroupLeader, setTasksForGroup methods, and methods for setting tasks in the Group class.

--- lesson14/src/main/java/incapsulation/Main.java
+++ lesson14/src/main/java/incapsulation/Main.java
@@ -31,5 +31,21 @@ public class Main {
         System.out.println("-------------");
         group1.printValue();
 
+        group1.changeGroupLeader(student2);
+        System.out.println("-------------");
+        group1.printValue();
+        Student student6 = new Student(678, "Stepan", "Bandera");
+        group1.addStudent(student6);
+        System.out.println("-------------");
+        group1.printValue();
+        group1.removeStudent(student4);
+        System.out.println("-------------");
+        group1.printValue();
+        group1.renameStudent(student1, "Tatiana");
+        group1.printValue();
+        System.out.println("-------------");
+
+
+
     }
 }
