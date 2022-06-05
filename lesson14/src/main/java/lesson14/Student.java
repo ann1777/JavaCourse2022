35b0fb0cb0792d402632530a3a66b21968678f0a
Angela Potapchuk <angela.harkov@gmail.com>
2022-06-05 22:19:43 +0300

    Created class Student, added constructors, setters and getters.

--- lesson14/src/main/java/lesson14/Student.java
+++ lesson14/src/main/java/lesson14/Student.java
@@ -1,4 +1,44 @@
 package lesson14;
 
+//class Student has private ID, name and lastname fields
 public class Student {
+    private int ID;
+    private String name;
+    private String lastname;
+    //constructor for class Student
+    public Student(int ID, String name, String lastname) {
+        this.ID = ID;
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    //empty constructor for class Student
+    public Student() {
+    }
+
+    //setters
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    //getters
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
 }
