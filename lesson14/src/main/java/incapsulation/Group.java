9119ae89bec5233049a2d2049d2364fa17d7342d
Angela Potapchuk <angela.harkov@gmail.com>
2022-06-06 00:25:02 +0300

    Class Group represents students group and has groupLeader, students list and listOfTasks fields created with constructors and methods (setStudents, setGroupLeader, setGroupID, printGroupID, printGroupLeader, printStudents and printValue). Students added to the ArayList and called group methods(setGroupID, printGroupID, setGroupLeader, setStudents, printValue)

---
+++ lesson14/src/main/java/incapsulation/Group.java
@@ -0,0 +1,63 @@
+package incapsulation;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+//class represents students group has groupLeader, students list and listOfTasks fields
+public class Group {
+    private int groupID;
+    private ArrayList<Student> students;
+    private Student groupLeader;
+    private Map<Integer, String> listOfTasks;
+    private LinkedHashMap<Integer, Map<Integer, Boolean>> studentTasks;
+
+    public Group(Student groupLeader, ArrayList<Student> students) {
+        this.students = students;
+        this.groupLeader = groupLeader;
+    }
+
+    public Group() {
+    }
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+    public void setGroupLeader(Student groupLeader) {
+        this.groupLeader = groupLeader;
+    }
+
+//    public void groupLeaderToGroup() {
+//        students.add(groupLeader);
+//    }
+
+    public void setGroupID(int groupID) {
+        this.groupID = groupID;
+    }
+
+    public void printGroupID() {
+        System.out.println("Group ID: "+ groupID);
+    }
+
+    public void printGroupLeader() {
+        System.out.println("Group Leader: " + groupLeader.getID() + " " +groupLeader.getName() + " " + groupLeader.getLastname());
+    }
+
+    public void printStudents() {
+        int i=1;
+        for (Student s : students) {
+            System.out.println("Student "+i+": "+s.getID()+" "+s.getName()+" "+s.getLastname());
+            i++;
+        }
+    }
+
+    public void printValue(){
+        printGroupID();
+        printGroupLeader();
+        printStudents();
+    }
+
+
+
+
+}
--- lesson14/src/main/java/incapsulation/Main.java
+++ lesson14/src/main/java/incapsulation/Main.java
@@ -1,5 +1,7 @@
 package incapsulation;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Student student1 = new Student(111, "Ivan", "Potap");
@@ -13,5 +15,21 @@ public class Main {
         System.out.println(student1.getID());
         System.out.println(student1.getName());
         System.out.println(student1.getLastname());
+
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        students.add(student4);
+        students.add(student5);
+
+        Group group1 = new Group();
+        group1.setGroupID(2);
+        group1.printGroupID();
+        group1.setGroupLeader(student1);
+        group1.setStudents(students);
+        System.out.println("-------------");
+        group1.printValue();
+
     }
 }
