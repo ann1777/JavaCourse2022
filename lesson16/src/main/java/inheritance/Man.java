33e0140630797eb2ac4cd8bea2fd32e269434fa6
Angela Potapchuk <angela.harkov@gmail.com>
2022-06-08 14:14:00 +0300

    Classes Man and Woman extends class Person. IsSmoking, grosSalary fields, and theirs setters were been added to the class Man. NumberOfChildren, isBlond and lostWeight fields and their setters were been added to the class Woman

--- lesson16/src/main/java/inheritance/Man.java
+++ lesson16/src/main/java/inheritance/Man.java
@@ -1,4 +1,20 @@
 package inheritance;
 
-public class Man {
+public class Man extends Person {
+    public boolean isSmoking;
+    protected double grosSalary;
+
+    public Man(String name, String lastname, int age, boolean isPartnerMale, boolean isSmoking, double salary) {
+        super(name, lastname, age, isPartnerMale);
+        this.isSmoking = isSmoking;
+        this.grosSalary = salary;
+    }
+
+    public void setSmoking(boolean smoking) {
+        isSmoking = smoking;
+    }
+
+    public void setGrosSalary(double grosSalary) {
+        this.grosSalary = grosSalary;
+    }
 }
--- lesson16/src/main/java/inheritance/Woman.java
+++ lesson16/src/main/java/inheritance/Woman.java
@@ -1,4 +1,39 @@
 package inheritance;
 
-public class Woman {
+public class Woman extends Person {
+    private int numberOfChildren;
+    protected boolean isBlond;
+    private double lostWeight;
+
+    public Woman(String name, String lastname, int age, boolean isPartnerMale, int numberOfChildren, boolean isBlond,
+                 double lostWeight) {
+        super(name, lastname, age, isPartnerMale);
+        this.numberOfChildren = numberOfChildren;
+        this.isBlond = isBlond;
+        this.lostWeight = lostWeight;
+    }
+
+    public int getNumberOfChildren() {
+        return numberOfChildren;
+    }
+
+    public double getLostWeight() {
+        return lostWeight;
+    }
+
+    public boolean isBlond() {
+        return isBlond;
+    }
+
+    public void setNumberOfChildren(int numberOfChildren) {
+        this.numberOfChildren = numberOfChildren;
+    }
+
+    public void setBlond(boolean blond) {
+        isBlond = blond;
+    }
+
+    public void setLostWeight(double lostWeight) {
+        this.lostWeight = lostWeight;
+    }
 }
