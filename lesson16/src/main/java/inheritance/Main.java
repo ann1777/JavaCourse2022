cba03f101646d5939aa8e8ce2bd8b78f46409656
Angela Potapchuk <angela.harkov@gmail.com>
2022-06-08 12:35:54 +0300

    Created class Person. In Person created constructor, getters, setters and isPartnerMale, isRetired, registerPartnership and deregisterPartnership methods

--- lesson16/src/main/java/inheritance/Main.java
+++ lesson16/src/main/java/inheritance/Main.java
@@ -1,7 +1,7 @@
 package inheritance;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
     }
 }
--- lesson16/src/main/java/inheritance/Person.java
+++ lesson16/src/main/java/inheritance/Person.java
@@ -1,4 +1,68 @@
 package inheritance;
 
-public class Person {
+//class Person has name, lastname, age and isPartnerMale fields
+public abstract class Person {
+    protected String name;
+    protected String lastname;
+    protected String maidenLastname;
+    protected int age;
+    private boolean isPartnerMale;
+    protected boolean isRetired;
+    protected boolean revertLastname;
+
+    //constructor for class Person
+    public Person(String name, String lastname, int age, boolean isPartnerMale) {
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+        this.isPartnerMale = isPartnerMale;
+    }
+
+    //add setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setPartnerMale(boolean partnerMale) {
+        isPartnerMale = partnerMale;
+    }
+
+    //add Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isPartnerMale() {
+        return isPartnerMale;//return true
+    }
+
+    //create isRetired method
+    public boolean isRetired() {
+        return isRetired;//return true
+    }
+
+    //create registerPartnership method
+    public void registerPartnership(String lastname) {
+    }
+
+    //create deregisterPartnership method
+    public void deregisterPartnership(boolean revertLastname, String maidenLastname) {
+    }
 }
+
