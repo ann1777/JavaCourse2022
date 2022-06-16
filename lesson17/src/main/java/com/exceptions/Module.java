43bbc5004c42ab2b1d41ec789cf88f97c784ca09
Angela Potapchuk <angela.harkov@gmail.com>
2022-06-16 18:52:11 +0300

    Catching all exceptions from main method

--- lesson17/src/main/java/com/exceptions/Module.java
+++ lesson17/src/main/java/com/exceptions/Module.java
@@ -31,7 +31,7 @@ public class Module {
         return "trx_4knfsf4gs412355";
     }
 
-    public static void main(String[] args) throws BankProcessingFailedException, InvalidPaymentCurrencyException, InvalidPaymentAmountException {
+    public static void main(String[] args) {
         System.out.println("\nBankProcessing1: Please input paymentAmount 20");
         System.out.println("BankProcessing1 is started with correct paymentAmount, without currency walue.");
         try {
@@ -60,7 +60,10 @@ public class Module {
             System.out.println("BankProcessing3 failed. Exception2: Connection with bank has interrupted. " + e3.getMessage());
         } catch (InvalidPaymentCurrencyException e4) {
             System.out.println("ProcessPayment3 failed. Exception3: " + e4.getMessage());
+        } catch (InvalidPaymentAmountException e41) {
+            System.out.println("ProcessPayment3 failed. Exception3.1: " + e41.getMessage());
         }
+
         System.out.println("\nBankProcessing4: Please input paymentAmount -20 JPY");
         System.out.println("BankProcessing4 is started with negative payment amount.");
         try {
@@ -72,13 +75,19 @@ public class Module {
             System.out.println("BankProcessing4 failed. Exception1: " + e1.getMessage());
         } catch (InvalidPaymentAmountException e5) {
             System.out.println("ProcessPayment3 failed. Exception5: " + e5.getMessage());
+        } catch (InvalidPaymentCurrencyException e51) {
+            System.out.println("ProcessPayment3 failed. Exception5.1: " + e51.getMessage());
         }
         System.out.println("\nBankProcessing5: Please input paymentAmount 0 EUR");
         System.out.println("BankProcessing5 is started with zero payment amount.");
         try {
             requestBankProcessing(0);
             System.out.println("BankProcessing5 was successful. ProcessPayment4 is started.");
-            processPayment(0, "EUR", "834");
+            try {
+                processPayment(0, "EUR", "834");
+            } catch (InvalidPaymentCurrencyException e) {
+                e.printStackTrace();
+            }
             System.out.println("ProcessPayment5 was Successfull");
         } catch (InvalidPaymentAmountException e7) {
             System.out.println("ProcessPayment4 failed. Exception7: " + e7.getMessage());
@@ -92,8 +101,12 @@ public class Module {
             System.out.println("BankProcessing6 was successful. ProcessPayment6 is started.");
             processPayment(100, "CHF", "153");
             System.out.println("Your processPayment6 was Successfull");
-        } catch (BankProcessingFailedException e1) {
+        } catch ( InvalidPaymentAmountException e1) {
             System.out.println("ProcessPayment6 failed. Exception2: Connection with bank has interrupted. " + e1.getMessage());
+        } catch (BankProcessingFailedException e11) {
+            System.out.println("ProcessPayment6 failed. Exception2.1: Connection with bank has interrupted. " + e11.getMessage());
+        } catch (InvalidPaymentCurrencyException e12) {
+            System.out.println("ProcessPayment6 failed. Exception2.2: Connection with bank has interrupted. " + e12.getMessage());
         }
     }
 }
