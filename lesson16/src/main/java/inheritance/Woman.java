52d4f58349b6abe211aa8051083e349620c9f710
angel <angela.harkov@gmail.com>
2022-06-25 15:24:22 +0300

    Debug TestPersonWoman methods, created all needs getters and setters in class Woman.

--- lesson16/src/main/java/inheritance/Woman.java
+++ lesson16/src/main/java/inheritance/Woman.java
@@ -1,50 +1,79 @@
-package inheritance;
+package lesson16.src.main.java.inheritance;
 
-public class Woman extends inheritance.Person {
+public class Woman extends Person {
+
+    protected double currentWeight;
     private int numberOfChildren;
 
-    public boolean isBlond() {
+    public boolean isBlond(boolean b) {
         return isBlond;
     }
 
     protected boolean isBlond;
+    protected double weight;
     private double lostWeight;
 
     public Woman(String name, String lastname, int age, boolean isPartnerMale, int numberOfChildren, boolean isBlond,
                  double lostWeight) {
         super(name, lastname, age, isPartnerMale);
-        this.numberOfChildren = numberOfChildren;
+        this.setNumberOfChildren(numberOfChildren);
         this.isBlond = isBlond;
-        this.lostWeight = lostWeight;
+        this.setLostWeight(lostWeight);
+    }
+    public Woman() {
+        super();
     }
 
     public int getNumberOfChildren() {
         return numberOfChildren;
+    }
+    public void setCurrentWeight(double weight) {
+        this.weight = weight;
     }
 
     public double getLostWeight() {
         return lostWeight;
     }
 
-    public boolean setIsBlond(boolean isBlond) {
-        if(setIsBlond(true))
-        {
-            System.out.println(true);
-            return true;
+    public boolean setIsBlond() {
+        System.out.println("----- START OF Verify function isBlond in TestClassWoman class -------------");
+        if (this.isBlond(false)){
+            try {
+                return this.isBlond(true);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
         }
-        else
-        {
-            System.out.println(false);
-            return false;
+        System.out.println("----- END OF Verify function isBlond in TestClassWoman class -------------");
+        return this.isBlond(true);
+    }
+    public void setNumberOfChildren(int numberOfChildren) {
+        System.out.println("----- START OF Verify function setNumberOfChildren in TestClassWoman class -------------");
+        if (this.age > 18) {
+            try {
+                this.numberOfChildren = numberOfChildren;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
         }
+        else{
+            System.out.println("She is too young woman to have children.");
+        }
+        System.out.println("----- END OF Verify function setNumberOfChildren in TestClassWoman class -------------");
     }
 
-    public void setNumberOfChildren(int numberOfChildren) {
-        this.numberOfChildren = numberOfChildren;
+    public void setWeight(double weight) {
+        this.weight = weight;
     }
 
     public void setLostWeight(double lostWeight) {
         this.lostWeight = lostWeight;
+    }
+    public double getCurrentWeight(double weight, double lostWeight) {
+        this.weight = weight;
+        this.setLostWeight(lostWeight);
+        double currentWeight = weight - lostWeight;
+        return currentWeight;
     }
 
     @Override
@@ -53,7 +82,7 @@ public class Woman extends inheritance.Person {
     }
 
     @Override
-    public void registerPartnership2(inheritance.Person partner, boolean isPartnerMale) {
+    public void registerPartnership2(lesson16.src.main.java.inheritance.Person partner, boolean isPartnerMale) {
         super.registerPartnership2(partner, isPartnerMale);
         if (isPartnerMale) {
             this.setLastname(partner.lastname);
--- testPersonInheritance/src/test/java/TestPersonWoman.java
+++ testPersonInheritance/src/test/java/TestPersonWoman.java
@@ -1,29 +1,25 @@
+package lesson20.testPersonInheritance.src.test.java;
+
 import org.testng.Assert;
 import org.testng.annotations.*;
 
 import java.util.HashMap;
 
-public class TestPersonWoman extends Woman {
+public class TestPersonWoman extends lesson16.src.main.java.inheritance.Woman {
     private static final TestPersonWoman woman1 = new TestPersonWoman();
     private static final TestPersonWoman woman2 = new TestPersonWoman();
     private final HashMap<TestPersonWoman, String> parseLocaleData = new HashMap<>();
-    private double setWeight;
 
     public TestPersonWoman(String name, String lastname, int age, boolean isPartnerMale, int numberOfChildren, boolean isBlond, double lostWeight) {
         super(name, lastname, age, isPartnerMale, numberOfChildren, isBlond, lostWeight);
-        this.name = name;
-        this.lastname = birthLastname;
-        this.age = age;
-        this.isBlond = isBlond;
     }
 
     public TestPersonWoman() {
-
+        super();
     }
 
-
     @BeforeClass
-    private void Setup(){
+    private void Setup() {
         parseLocaleData.put(woman1, TestPersonWoman.getWoman1().toString());
         parseLocaleData.put(woman2, TestPersonWoman.getWoman2().toString());
         System.out.println("This is executed before each Class");
@@ -51,48 +47,15 @@ public class TestPersonWoman extends Woman {
     private static Object getWoman2() {
         return woman2;
     }
-    private boolean isBlond(boolean isBlond) {
+    public boolean isBlond(boolean isBlond) {
         if (!isBlond) {
             return false;
         } else {
             return true;
         }
     }
-
     public HashMap<TestPersonWoman, String> getParseLocaleData() {
         return parseLocaleData;
-    }
-    public boolean setIsBlond() {
-        System.out.println("----- START OF Verify function isBlond in TestClassWoman class -------------");
-        if (this.isBlond(false)){
-            try {
-                return this.isBlond(true);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
-        System.out.println("----- END OF Verify function isBlond in TestClassWoman class -------------");
-        return this.isBlond(true);
-    }
-
-    public int setNumberOfChildren(int age, int numberOfChildren) {
-        System.out.println("----- START OF Verify function setNumberOfChildren in TestClassWoman class -------------");
-        if (this.age > 18) {
-            try {
-                this.setNumberOfChildren(numberOfChildren);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
-        else{
-            System.out.println("She is too young woman to have children.");
-            }
-            System.out.println("----- END OF Verify function setNumberOfChildren in TestClassWoman class -------------");
-            return numberOfChildren;
-    }
-
-    public int getNumberOfChildren() {
-        return this.numberOfChildren;
     }
 
     @Test(description = "this is check if setIsBlond return isBlond")
@@ -107,53 +70,43 @@ public class TestPersonWoman extends Woman {
     }
 
     @Test(description = "this is check if setNumberOfChildren works")
-    public void setNumberOfChildren(){
+    public void setNumberOfChildren() {
         System.out.println("----- START OF testing function setNumberOfChildren in TestClassWoman class -------------");
         woman1.setAge(15);
-        System.out.println("Woman1 is "+woman1.getAge()+" age old");
-        woman1.setNumberOfChildren(16, 2);
-        System.out.println("She has "+woman1.getNumberOfChildren()+" children");
+        System.out.println("Woman1 is " + woman1.getAge() + " age old");
+        woman1.setNumberOfChildren(2);
+        System.out.println("She has " + woman1.getNumberOfChildren() + " children");
         woman2.setAge(22);
-        System.out.println("Woman2 is "+woman2.getAge()+" age old");
-        woman2.setNumberOfChildren(22, 4);
-        System.out.println("She has already "+woman2.getNumberOfChildren()+" children");
+        System.out.println("Woman2 is " + woman2.getAge() + " age old");
+        woman2.setNumberOfChildren(4);
+        System.out.println("She has already " + woman2.getNumberOfChildren() + " children");
         System.out.println("----- END OF testing function setNumberOfChildren in TestClassWoman class -------------");
     }
 
     @Test(description = "this is check if isRetired works for class Woman")
-    public void setIsRetired(){
+    public void setIsRetired() {
         System.out.println("----- START OF testing function isRetired in TestClassWoman class -------------");
         woman1.setAge(18);
-        System.out.println("Woman1 is "+woman1.getAge()+" age old");
+        System.out.println("Woman1 is " + woman1.getAge() + " age old");
         woman1.isRetired();
         woman2.setAge(60);
-        System.out.println("Woman2 is "+woman2.getAge()+" age old");
+        System.out.println("Woman2 is " + woman2.getAge() + " age old");
         woman2.isRetired();
         woman2.setAge(59);
-        System.out.println("Woman2 is "+woman2.getAge()+" age old");
+        System.out.println("Woman2 is " + woman2.getAge() + " age old");
         woman2.isRetired();
         System.out.println("----- END OF testing function isRetired in TestClassWoman class -------------");
-
-    }
-
-    @Test(description = "this is check if setLostWeight works for class Woman")
-    public void setLostWeight() {
-        System.out.println("----- START OF testing function setLostWeight in TestClassWoman class -------------");
-        woman1.weight = (double) 55.00;
-        System.out.println("This woman had have " + woman1.weight + " kg of weight");
-        woman1.setLostWeight(12.4);
-        System.out.println("She had lost " + 12.4 + " kg of weight");
-        System.out.println("----- END OF testing function setLostWeight in TestClassWoman class -------------");
     }
 
     @Test(description = "this is check if getCurrentWeight works for class Woman")
     public void getCurrentWeight() {
         System.out.println("----- START OF testing function getCurrentWeight in TestClassWoman class -------------");
-        woman1.setWeight(55.00);
-        System.out.println("A few months ago she has "+woman1.getWeight()+" kg of weight");
+        woman1.setCurrentWeight(55.00);
+        System.out.println("A few months ago she has " + woman1.weight + " kg of weight");
         woman1.getCurrentWeight(55.00, 12.40);
-        System.out.println("After she lost 12.4 kg she has "+ woman1.getCurrentWeight(55.00, 12.40) + " kg of weight now");
+        System.out.println("After she lost 12.4 kg she has " + woman1.getCurrentWeight(55.00, 12.40) + " kg of weight now");
+        Assert.assertEquals(woman1.getCurrentWeight(55.00, 12.40), 42.6);
         System.out.println("----- END OF testing function getCurrentWeight in TestClassWoman class -------------");
     }
-
 }
+
