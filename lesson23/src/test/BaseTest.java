3f8b87950bee99dafc02ea1a1791352d09c0374a
Angela Potapchuk <angela.harkov@gmail.com>
2022-07-06 12:53:25 +0300

    Added getConnection, getInstance and closeConnection methods to the DBConnection class and test it from the BaseTest class. Configured pom.xml file

--- lesson23/src/test/BaseTest.java
+++ lesson23/src/test/BaseTest.java
@@ -1,6 +1,12 @@
 package test;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class BaseTest {
     public static String dbServerAddress = "jdbc:postgresql://localhost:5432/";
@@ -9,5 +15,34 @@ public class BaseTest {
     public static String dbPassword = "angel";
 
     private static final Logger log = LogManager.getLogger(BaseTest.class);
+
+    protected Connection getConnectionDB() throws SQLException {
+        try {
+            return DBConnection.getInstance(dbAddress, dbUser, dbPassword).getConnection();
+        } catch (SQLException e) {
+            log.error("Failed to get DBConnection\n {}", e.getMessage());
+        }
+        return null;
+    }
+
+    @BeforeSuite
+    protected Connection getConnectionServer() {
+        try {
+            return DBConnection.getInstance(dbServerAddress, dbUser, dbPassword).getConnection();
+        } catch (SQLException ex) {
+            log.error("Failed to get ServerConnection\n {}", ex.getMessage());
+        }
+        return null;
+    }
+
+
+    @AfterSuite
+    public void tearDown() {
+        try {
+            DBConnection.closeConnection();
+        } catch (SQLException exc) {
+            log.error("Failed to close connection {}", exc.getMessage());
+        }
+    }
 }
 
--- lesson23/src/test/DBConnection.java
+++ lesson23/src/test/DBConnection.java
@@ -1,4 +1,5 @@
 package test;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,7 +9,7 @@ import java.util.logging.Logger;
 public class DBConnection {
     private static DBConnection instance;
     private static Connection dbConnection;
-    private final String dbUrl;
+    private static String dbUrl;
     private final String dbUser;
     private static final Logger log = LogManager.getLogManager().getLogger(String.valueOf(DBConnection.class));
 
@@ -16,5 +17,29 @@ public class DBConnection {
         dbConnection = DriverManager.getConnection(url, dbUser, dbPassword);
         this.dbUrl = url;
         this.dbUser = dbUser;
+    }
+
+    public static Connection getConnection() {
+        return dbConnection;
+    }
+
+    public static DBConnection getInstance(String dbAddress, String dbUser, String dbPassword) throws SQLException {
+        DBConnection instance = null;
+        if (instance == null) {
+            instance = new DBConnection(dbUrl, dbUser, dbPassword);
+        } else if (!instance.equals(dbUser)) {
+            log.warning("Connection is open with wrong credentials");
+        }
+        return instance;
+    }
+
+    public static void closeConnection() {
+        if (instance != null) {
+            try {
+                dbConnection.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
