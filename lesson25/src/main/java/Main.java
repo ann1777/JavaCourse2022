32efa11e32cb6716056cfb6a6d980ea831a638c3
Angela Potapchuk <angela.harkov@gmail.com>
2022-07-11 09:32:23 +0300

    Initial commit

---
+++ lesson25/src/main/java/Main.java
@@ -0,0 +1,8 @@
+import java.security.Provider;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi everyone!");
+
+    }
+}
---
+++ lesson25/src/test/java/DemoqaButtonsPage.java
@@ -0,0 +1,4 @@
+
+public class DemoqaButtonsPage extends DemoqaElementsPage {
+
+}
