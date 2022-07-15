8d66b2cf065c362679da6966376968bf6f11be39
Angela Potapchuk <angela.harkov@gmail.com>
2022-07-15 13:40:16 +0300

    Created editsParsonFields metod with asserts in the TestDemoqacom class

--- lesson25/src/test/java/TestDemoqacom.java
+++ lesson25/src/test/java/TestDemoqacom.java
@@ -132,4 +132,54 @@ public class TestDemoqacom {
         WebElement fieldHasBeenAdded = driver.findElement(By.xpath("//*[text()='Marry']"));
         Assert.assertEquals(fieldHasBeenAdded.getText(), "Marry", "New entry has not been added to the users table");
     }
+    public void editsParsonFields() {
+        //initialise new chromedriver
+        System.clearProperty("webdriver.chrome.driver");
+        System.setProperty("webdriver.chrome.driver", "/home/angel/ideaProjects/test/lesson25/chromedriver");
+        System.setProperty("webdriver.chrome.whitelistedIps", "");
+        ChromeOptions option = new ChromeOptions();
+        WebDriver driver = new ChromeDriver(option);
+        driver.manage().window().maximize();
+        driver.get("https://demoqa.com/webtables");
+        WebElement title = driver.findElement(By.xpath("//div[text()='Web Tables']"));
+        Assert.assertTrue(title.isDisplayed());
+
+        // edits the entry through the edit function
+        WebElement editIcon = driver.findElement(By.xpath("//div[text()='Marry']/../..//div[@class=\"action-buttons\"]//span[@title=\"Edit\"]"));
+        editIcon.click();
+
+        WebElement newModalTitle = driver.findElement(By.xpath("//*[@id=\"registration-form-modal\"]"));
+        Assert.assertEquals(newModalTitle.getText(), "Registration Form", " Title Registration Form is absent on the modal window");
+//        assert newModalTitle.getText().equals("Registration Form");
+
+        WebElement firstNameFld = driver.findElement(By.xpath("//input[@value = \"Marry\"]"));
+        firstNameFld.sendKeys("Garry");
+        Assert.assertEquals(firstNameFld.getText(), "Garry", "Field name has not been modified successful");
+//        assert firstNameFld.getText().equals("Garry");
+
+        WebElement lastNameFld = driver.findElement(By.xpath("//input[@value = \"Poppins\"]"));
+        lastNameFld.sendKeys("Potter");
+        Assert.assertEquals(lastNameFld.getText(), "Potter", "Field lastname has not been modified successful");
+//        assert lastNameFld.getText().equals("Potter");
+
+        WebElement emailFld = driver.findElement(By.xpath("//input[@value = \"marry@gmail.com\"]"));
+        emailFld.sendKeys("garry@i.ua");
+        Assert.assertEquals(firstNameFld.getText(), "Potter", "Field lastname has not been modified successful");
+//        assert emailFld.getText().equals("garry@i.ua");
+
+        WebElement ageFld = driver.findElement(By.xpath("//input[@id=\"age\"]"));
+        ageFld.sendKeys("14");
+        Assert.assertEquals(ageFld.getText(), "14", "Field ageFld has not been modified successful");
+//        assert ageFld.getText().equals("14");
+
+        WebElement salaryFld = driver.findElement(By.xpath("//input[@id=\"salary\"]"));
+        salaryFld.sendKeys("99999990000000");
+        Assert.assertEquals(salaryFld.getText(), "99999990000000", "Field salaryFld has not been modified successful");
+//        assert salaryFld.getText().equals("99999990000000");
+
+        WebElement departmentFld = driver.findElement(By.xpath("//input[@placeholder=\"Department\"]"));
+        departmentFld.sendKeys("Hogwarts magic");
+        Assert.assertEquals(departmentFld.getText(), "Hogwarts magic", "Field departmentFld has not been modified successful");
+//        assert departmentFld.getText().equals("Hogwarts magic");
+    }
 }
