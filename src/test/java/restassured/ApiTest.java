83997d14c3f61f62707f0d382ce77b16669631a7
Angela Potapchuk <angela.harkov@gmail.com>
2022-08-22 23:51:29 +0300

    Created testStudentSearch and testStudentDashboard methods to the ApiTest class

--- src/test/java/restassured/ApiTest.java
+++ src/test/java/restassured/ApiTest.java
@@ -7,6 +7,9 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ApiTest extends BaseTest {
     @Test
@@ -48,6 +51,54 @@ public class ApiTest extends BaseTest {
         Student[] allStudents = RestAssured.get("/students").as(Student[].class);
         Assert.assertEquals(allStudents.length, 3);
         System.out.println(Arrays.asList(allStudents));
+    }
+    @Test
+    public void testStudentSearch() {
+        RestAssured.given()
+                .body(new StudentData("Oleksandr", "Svidersky"))
+                .post("/students")
+                .then().statusCode(200);
+        RestAssured.given()
+                .body(new StudentData("Angela", "Potapchuk"))
+                .post("/students")
+                .then().statusCode(200);
+        RestAssured.given()
+                .body(new StudentData("Volodymyr", "Golovach"))
+                .post("/students")
+                .then().statusCode(200);
+        Response response = RestAssured.given()
+                .queryParam("last_name", "ch")
+                .when().get("/students");
+        response.then().statusCode(200);
+        Student[] searchResult = response.as(Student[].class);
+        Assert.assertEquals(searchResult.length, 2);
+        Set<String> names = Arrays.stream(searchResult).map(s -> s.firstName).collect(Collectors.toSet());
+        Assert.assertEquals(names, Set.of("Angela", "Volodymyr"));
+    }
+
+    @Test
+    public void testStudentDashboard() {
+        RestAssured.given()
+                .body(new StudentData("Ivan", "Litvinau"))
+                .post("/students")
+                .then().statusCode(200);
+        RestAssured.given()
+                .body(new StudentData("Dmytro", "Bidenko"))
+                .post("/students")
+                .then().statusCode(200);
+        RestAssured.given()
+                .body(new StudentData("Valentyna", "Mykhaiova"))
+                .post("/students")
+                .then().statusCode(200);
+
+        List<Student> allStudents = Arrays.asList(
+                RestAssured.get("/students")
+                        .then().statusCode(200)
+                        .extract().as(Student[].class)
+        );
+        List<Integer> studentIds = allStudents.stream().map(student -> student.id).collect(Collectors.toList());
+        Assert.assertNotNull(studentIds.size());
+        System.out.println(studentIds.size());
     }
 
 
