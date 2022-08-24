dc6972cd9a51a120bed5b4377d76354edea9b007
Angela Potapchuk <angela.harkov@gmail.com>
2022-08-24 13:17:48 +0300

    Added variables to the StudentDashboardAssignment class

--- src/main/java/restassured/StudentDashboardAssignment.java
+++ src/main/java/restassured/StudentDashboardAssignment.java
@@ -1,4 +1,17 @@
 package restassured;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class StudentDashboardAssignment {
+    @JsonProperty
+    public Integer id;
+
+    @JsonProperty
+    public AssignmentContent content;
+
+    @JsonProperty
+    public String solution;
+
+    @JsonProperty
+    public Integer mark;
 }
